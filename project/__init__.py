2c4690de0320c42a17e3cf165a17c4dbb6167832
Rob Conner <rjconner@mac.com>
2017-09-03 14:18:11 -0700

    initial commit

---
+++ project/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_modus import Modus
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+modus = Modus(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+# blueprints must be imported after app initialization
+from project.users.views import users_blueprint
+from project.workouts.views import workouts_blueprint
+from project.exercises.views import exercises_blueprint
+from project.tags.views import tags_blueprint
+
+# register blueprints with the application
+app.register_blueprint(users_blueprint, url_prefix='/users')
+app.register_blueprint(workouts_blueprint, url_prefix='/users/<int:user_id>/workouts')
+app.register_blueprint(exercises_blueprint, url_prefix='/users/<int:user_id>/workouts/<int:workout_id>/exercises')
+app.register_blueprint(tags_blueprint, url_prefix='/tags')
+
+# redirect on attempt to access a private route
+login_manager.login_view = 'users.login'
+
+from project.models import User
+
+# write a method with the user_loader decorator so that flask_login can find a current_user 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+@app.route('/')
+def root():
+    return redirect(url_for('users.login'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
