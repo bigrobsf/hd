25750094307dade32c125d8fdbf3f176e283104a
Rob Conner <rjconner@mac.com>
2017-09-05 11:45:33 -0700

    edits to workout forms

--- project/workouts/forms.py
+++ project/workouts/forms.py
@@ -1,18 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, DateField, DecimalField, FormField
 from project.models import Workout, Activity, Exercise
-from project.activities.form import ActivityForm
 
 
 class WorkoutForm(FlaskForm):
     location = StringField('Location', [validators.Length(min=1, max=30)])
     comment = StringField('Comment', [validators.Length(max=50)])
 
+class ActivityForm(FlaskForm):
+    reps = DecimalField('Reps', [validators.NumberRange(min=0, max=50)])
+    weight = DecimalField('Weight', [validators.NumberRange(min=0, max=1000)])
+    comment = StringField('Comment', [validators.Length(max=50)])
+    
 class EditWorkoutForm(FlaskForm):
     location = StringField('Location', [validators.Length(min=1, max=30)])
     length = DecimalField('Workout Length', [validators.NumberRange(min=1, max=60)])
     comment = StringField('Comment', [validators.Length(max=50)])
-    activity = [FormField(ActivityForm, e.name) for e in Exercise.query.all()]
+    activities = [FormField(ActivityForm, e.name) for e in Exercise.query.all()]
 
 class DeleteForm(FlaskForm):
     pass
